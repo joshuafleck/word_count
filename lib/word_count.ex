7fd0e0595107fbab765b6c694882101f2c7f135f
Joshua Fleck <flec0025@umn.edu>
2017-09-26 15:01:12 +0100

    Initial commit

---
+++ lib/word_count.ex
@@ -0,0 +1,18 @@
+defmodule WordCount do
+  @moduledoc """
+  Documentation for WordCount.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WordCount.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
